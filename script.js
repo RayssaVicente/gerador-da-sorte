4d30f8c27aa5a1c63a20a4c03505fab5a0855b6d
Luan <luancarloslaurentino52@gmail.com>
2025-10-30 22:11:28 -0300

    Cria função 'gerarNumerosUnicos'

--- script.js
+++ script.js
@@ -2,15 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const botaoGerar = document.getElementById('btn-gerar');
     const seletorJogo = document.getElementById('tipo-jogo');
     const areaResultado = document.getElementById('resultado');
+
     const regras = {
         'mega-sena': { quantidade: 6, maximo: 60 },
         'quina': { quantidade: 5, maximo: 80 },
         'lotofacil': { quantidade: 15, maximo: 25 },
         'lotomania': { quantidade: 50, maximo: 100 }
     };
+
     botaoGerar.addEventListener('click', () => {
         const tipo = seletorJogo.value;
         const { quantidade, maximo } = regras[tipo];
     });
+        
+    /**
+     * 
+     * @param {number} quantidade 
+     * @param {number} maximo 
+     * @returns {number[]} 
+     */
+    function gerarNumerosUnicos(quantidade, maximo) {
+
+    }
     
 });
