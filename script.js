a64ce0217bd5e614fd29a5eda1548f5303718c0d
Luan <luancarloslaurentino52@gmail.com>
2025-10-30 21:49:35 -0300

    Cria objeto 'regras' para os jogos

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const botaoGerar = document.getElementById('btn-gerar');
     const seletorJogo = document.getElementById('tipo-jogo');
     const areaResultado = document.getElementById('resultado');
+    const regras = {
+
+    };
 });
