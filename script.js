3b4c48b42bfaaf04ed4d88007a5080c45f5e8226
Luan <luancarloslaurentino52@gmail.com>
2025-10-30 21:47:52 -0300

    Seleciona os  elementos do DOM (botão, select, resultado)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const botaoGerar = document.getElementById('btn-gerar');
+    const seletorJogo = document.getElementById('tipo-jogo');
+    const areaResultado = document.getElementById('resultado');
 });
