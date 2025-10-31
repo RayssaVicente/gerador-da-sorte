c4bc6611892bfd2024748ccac368bedabba8a69f
Luan <luancarloslaurentino52@gmail.com>
2025-10-30 21:51:36 -0300

    Defini regras para Mega Sena, Quina, Lotofácil e Lotomania

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const seletorJogo = document.getElementById('tipo-jogo');
     const areaResultado = document.getElementById('resultado');
     const regras = {
-
+        'mega-sena': { quantidade: 6, maximo: 60 },
+        'quina': { quantidade: 5, maximo: 80 },
+        'lotofacil': { quantidade: 15, maximo: 25 },
+        'lotomania': { quantidade: 50, maximo: 100 }
     };
 });
